8f914af8d0fc81fbca1317f70809b6f2b5e88afc
rahul.saini <rahul.saini@RYSUNLAP5381>
2024-12-19 13:21:33 +0530

    app Ignited with sufficient libraries

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const heading1 = React.createElement("h3", { id: "heading" }, "Hello World From React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", { id: "parent" },
+const heading = React.createElement("h3", { id: "heading" }, "Hello World From Rahul!");
+
+const heading1 = React.createElement("div", { id: "parent" },
     React.createElement("div", { id: "child" }, React.createElement("h3", { id: "heading" }, "I am H1 Tag!"))
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 
 root.render(heading);
